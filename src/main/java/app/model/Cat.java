99cf5ebb8fc04233515a84235980e6bb03241add
sinenkov <107352549+sinenkov@users.noreply.github.com>
2022-08-12 09:52:35 +0500

    2

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class Cat extends Animal {
     @Override
     public String toString() {
+        System.out.println("mew");
         return "Im a Cat";
     }
 }
